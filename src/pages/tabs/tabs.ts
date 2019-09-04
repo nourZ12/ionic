2cd4e7e36204b3f2b854b2d91856f35778113ab2
nourZ12 <nourzalatimo98@gmail.com>
2019-09-04 17:04:48 +0300

    changes

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, Tabs } from 'ionic-angular';
+import { FavoritesPage } from '../favorites/favorites';
+import { LibraryPage } from '../library/library';
+
+@Component({
+  selector: 'page-tabs',
+  template: `
+<ion-tabs selectedIndex="1">
+<ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star"></ion-tab>
+<ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
+</ion-tabs>
+`
+})
+export class TabsPage {
+  favoritesPage = FavoritesPage;
+  libraryPage = LibraryPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
